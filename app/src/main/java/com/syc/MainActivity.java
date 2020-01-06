513a712b1a49bfe4fde25c62dde12ccf1c30b20a
Taz91 <sylvain.chazette@orange.fr>
2020-01-06 03:20:24 +0100

    Initiate SearchActivity, intent param to Search and intent param to back MainActivity

--- app/src/main/java/com/syc/MainActivity.java
+++ app/src/main/java/com/syc/MainActivity.java
@@ -18,19 +18,16 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView content;
     private String typeNews;
     private static String API_KEY = "J0iJw0a8fdshubHztJsOJxEEg6hPstOG";
-
-    //Resources res = getResources();
-    //private static final String API_KEY = Context.get  //.getResources().getString(R.string.news_api_key);
-    //String API_KEY = res.getString(R.string.news_api_key);
-
     @BindView(R.id.main_news) CoordinatorLayout mainNews;
     @BindView(R.id.appbarlayout) AppBarLayout appBarLayout;
     @BindView(R.id.toolbar) Toolbar toolbar;
-
     @BindView(R.id.view_pager) ViewPager viewPager;
     @BindView(R.id.tabs) TabLayout tabs;
-    //@BindView(R.id.fab) FloatingActionButton fab;
 
+    //@BindView(R.id.fab) FloatingActionButton fab;
+    //Resources res = getResources();
+    //private static final String API_KEY = Context.get  //.getResources().getString(R.string.news_api_key);
+    //String API_KEY = res.getString(R.string.news_api_key);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,8 +78,15 @@ public class MainActivity extends AppCompatActivity {
 
     //optionMenu Notification
     private void callNotificationParams(){
-
         Intent searchActivityIntent = new Intent(MainActivity.this, SearchActivity.class);
+        //TODO: ici mettre le code pour récupérer les préférences et mettre ces valeurs par défaut au niveau du form search
+        //
+        //
+        String maString = "word1 word2 word3 etc";
+        searchActivityIntent.putExtra("wordDefault", maString);
+        searchActivityIntent.putExtra("cbArts", true);
+        searchActivityIntent.putExtra("cbSport", true);
+        searchActivityIntent.putExtra("bNotif", true);
         startActivity(searchActivityIntent);
     }
 
--- app/src/main/java/com/syc/SearchActivity.java
+++ app/src/main/java/com/syc/SearchActivity.java
@@ -1,20 +1,61 @@
 package com.syc;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class SearchActivity extends AppCompatActivity {
     @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.searchactivity_appbarlayout) AppBarLayout searchactivity_appbarlayout;
+    @BindView(R.id.searchactivity_toolbar) Toolbar searchactivity_toolbar;
+    @BindView(R.id.searchactivity_go) Button searchactivity_go;
+    @BindView(R.id.searchactivity_Text) EditText searchactivity_text;
+    @BindView(R.id.cb_arts) CheckBox cbArts;
+    @BindView(R.id.cb_sports) CheckBox cbSport;
+    @BindView(R.id.notif_go) Switch notifGo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
+
+        //get putExtra of MainActivity
+        Intent intent = getIntent();
+        if(intent != null  ){
+            if(intent.hasExtra("bNotif") && intent.getBooleanExtra("bNotif", false)){
+                //notifGo.setVisibility(View.INVISIBLE);
+                searchactivity_text.setText(intent.getStringExtra("wordDefault"));
+                searchactivity_go.setVisibility(View.INVISIBLE);
+            }
+            //
+
+
+            if(intent.hasExtra("wordDefault")){
+                searchactivity_text.setText(intent.getStringExtra("wordDefault"));
+            }
+            if(intent.hasExtra("cbArts")){
+                cbArts.setChecked(intent.getBooleanExtra("cbArts", false));
+            }
+            if(intent.hasExtra("cbSport")){
+                cbSport.setChecked(intent.getBooleanExtra("cbSport", false));
+            }
+
+        }
+        //searchactivity_toolbar.setDisplayShowHomeEnabled(true);
+        //searchactivity_toolbar.setDisplayHomeAsUpEnabled(true);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +64,27 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        searchactivity_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO : faire code du retour search, tester si back ou search
+                //TODO : si back, pas de vérif pas d'enregistrement, fin activity et retour mainActivity
+                //TODO : si search, alors tester si valeur obligatoire sont ok
+                //TODO : doit on tester s'il n'y a pas de retour de Response ? (car si vide le RV sera vide ... et donc prévenir avant d'y retourner de changer des choses .
+                //TODO : DatePicker ou autre format ? spinner date ou list de date ?
+                //
+                // code pour récup les saisies
+                Intent back = new Intent();
+                back.putExtra("cbArts", cbArts.isChecked());
+                back.putExtra("cbSport", cbSport.isChecked());
+                back.putExtra("wordDefault", searchactivity_text.getText() );
+
+            }
+        });
+
+
+
+
     }
 }
